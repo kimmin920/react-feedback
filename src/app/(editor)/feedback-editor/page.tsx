7c637899ab0c9e8dab57ad3a1fdd5368ff10870b
minwooKim <vvee1253@gmail.com>
2024-06-17 21:21:26 +0900

    feat: theme customizer

--- src/app/(editor)/feedback-editor/page.tsx
+++ src/app/(editor)/feedback-editor/page.tsx
@@ -10,6 +10,13 @@ import {
 } from '@/components/ui/resizable';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
+import { ThemeCustomizer } from '@/components/theme-customizer';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { ThemeWrapper } from '@/components/theme-wrapper';
+import { Button } from '@/components/ui/button';
+import { ThemeProvider, useTheme } from 'next-themes';
+import { useConfig } from '@/hooks/use-config';
+import { themes } from '@/registry/themes';
 
 export default function FeedbackEditorPage() {
   const [actionDesignType, setActionDesignType] =
@@ -19,31 +26,47 @@ export default function FeedbackEditorPage() {
     setActionDesignType(value);
   };
 
+  const { theme: mode } = useTheme();
+  const [config] = useConfig();
+
+  const theme = themes.find((theme) => theme.name === config.theme);
+
   return (
     <ResizablePanelGroup direction='horizontal' className='rounded-lg border'>
       <ResizablePanel defaultSize={25} minSize={5}>
-        <div className='flex h-full items-center justify-center p-6'>
-          <RadioGroup defaultValue='FACE' onValueChange={onChangeActionDesign}>
-            <div className='flex items-center space-x-2'>
-              <RadioGroupItem value='FACE' id='FACE' />
-              <Label htmlFor='FACE'>FACES</Label>
-            </div>
-            <div className='flex items-center space-x-2'>
-              <RadioGroupItem value='STAR' id='STAR' />
-              <Label htmlFor='STAR'>STARS</Label>
-            </div>
-          </RadioGroup>
-        </div>
+        <ThemeWrapper>
+          <div className='flex h-full items-center justify-center p-6'>
+            <RadioGroup
+              defaultValue='FACE'
+              onValueChange={onChangeActionDesign}
+            >
+              <div className='flex items-center space-x-2'>
+                <RadioGroupItem value='FACE' id='FACE' />
+                <Label htmlFor='FACE'>FACES</Label>
+              </div>
+              <div className='flex items-center space-x-2'>
+                <RadioGroupItem value='STAR' id='STAR' />
+                <Label htmlFor='STAR'>STARS</Label>
+              </div>
+            </RadioGroup>
+
+            <TooltipProvider>
+              <ThemeCustomizer />
+            </TooltipProvider>
+          </div>
+        </ThemeWrapper>
       </ResizablePanel>
       <ResizableHandle withHandle />
       <ResizablePanel defaultSize={75}>
-        <div className='flex h-full items-center justify-center p-6'>
-          <Feedback
-            designConfig={{
-              actions: actionDesignType,
-            }}
-          />
-        </div>
+        <ThemeWrapper>
+          <div className='flex h-full items-center justify-center p-6'>
+            <Feedback
+              designConfig={{
+                actions: actionDesignType,
+              }}
+            />
+          </div>
+        </ThemeWrapper>
       </ResizablePanel>
     </ResizablePanelGroup>
   );
--- src/app/(editor)/layout.tsx
+++ src/app/(editor)/layout.tsx
@@ -1,3 +1,5 @@
+import '../../../public/registry/themes.css';
+
 export default function EditorLayout({
   children,
 }: Readonly<{
